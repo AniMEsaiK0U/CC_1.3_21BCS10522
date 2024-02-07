47e9d785efb5eaa98baa6c56927e12fb33b5ea28
Ankit Chauhan <97604948+AniMEsaiK0U@users.noreply.github.com>
2024-02-07 10:27:17 +0530

    example.py

--- example.py
+++ example.py
@@ -1,4 +1,6 @@
-num1 = int(input(' any number -'))
-num2 = int(input('any number -'))
+print(' ')
+print('Program to add two numbers')
+num1 = int(input(' Enter Any Number - '))
+num2 = int(input('Enter any Number - '))
 sum = num1+num2
-print(sum)
+print('Sum of numbers is ',sum)
